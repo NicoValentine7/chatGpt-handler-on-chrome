7a6161cb26db5ceed32e8a2c3dc992f49024739e
Nico <50232024+NicoValentine7@users.noreply.github.com>
2022-12-07 18:11:13 +0900

    handle areaでcmd + enterすると内容をコピーしてfocusをメインのtextareに向ける

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+var form = document.querySelector("form");
+var textarea = document.createElement("textarea");
 addHandleTextarea();
 
 /**
@@ -20,8 +22,6 @@ if (div) {
  * form属性の上にtextareaを追加する<textarea placeholder="This is an ChatGpt Handler" rows="20" name="comment[text]" id="chat-gpt-handler" cols="40" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
  */
 function addHandleTextarea() {
-  var form = document.querySelector("form");
-  var textarea = document.createElement("textarea");
   textarea.setAttribute("placeholder", "This is an ChatGpt Handler.");
   textarea.setAttribute("rows", "20");
   textarea.setAttribute("name", "comment[text]");
@@ -33,4 +33,30 @@ function addHandleTextarea() {
   textarea.setAttribute("aria-autocomplete", "list");
   textarea.setAttribute("aria-haspopup", "true");
   form.parentElement.insertBefore(textarea, form);
+}
+
+/**
+ *
+ */
+
+// idがchat-gpt-handlerのtextareaでcmd+enterが押されたら入力されている内容をすべてコピーして, textareaを空にする。その後, data-idがrootのtextareaにフォーカスを当てて, ペーストする。
+textarea.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13 && e.metaKey) {
+    copyText();
+    textarea.value = "";
+    document.querySelector("textarea[data-id='root']").focus();
+    //0.5秒待ってからペーストする
+    setTimeout(function () {
+      document.execCommand("paste");
+    }, 500);
+  }
+});
+
+function copyText() {
+  // textarea要素を取得する
+  var textarea = document.getElementById("chat-gpt-handler");
+
+  // textarea要素の内容をクリップボードにコピーする
+  textarea.select();
+  document.execCommand("copy");
 }
